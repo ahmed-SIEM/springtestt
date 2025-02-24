e90c82136dc24ccfe4fb18d72dd1cf3e92de278e
Souid Ahmed <ahmedsouid008@gmail.com>
2025-02-24 09:57:02 +0100

    change type of the fetc from eager to lazy

--- src/main/java/com/example/springtestt/repositories/ClientRepository.java
+++ src/main/java/com/example/springtestt/repositories/ClientRepository.java
@@ -10,11 +10,4 @@ import java.util.List;
 
 public interface ClientRepository extends JpaRepository<Client,Long> {
 
-    // 1.1 Liste des commandes d'un client
-    List<Commande> findByClientIdClient(Long idClient);
-
-    // 1.2 Liste des commandes d'un client dont la date commande est entre deux dates
-    List<Commande> findByClientIdClientAndDateCommandeBetween(Long idClient, LocalDate startDate, LocalDate endDate);
-    // 1.3 Liste des commandes dont la date commande est entre deux dates ordonnées par prix croissant
-    List<Commande> findByDateCommandeBetweenOrderByTotalCommandeAsc(LocalDate startDate, LocalDate endDate);
 }
