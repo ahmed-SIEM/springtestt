7dbbdca95bda545c95080499c0896c6306726218
Souid Ahmed <ahmedsouid008@gmail.com>
2025-07-17 11:15:18 +0100

    cc

--- src/main/java/com/example/springtestt/controller/ClientController.java
+++ src/main/java/com/example/springtestt/controller/ClientController.java
@@ -30,6 +30,9 @@ public class ClientController {
 
 
 
+
+    
+
     @GetMapping("/retrieve-client/{client-id}")
     public Client retrieveClient(@PathVariable("client-id") Long clientId) {
         return (Client) clientService.retrieveClient(clientId);
